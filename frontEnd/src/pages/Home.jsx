09d5c63206ef5fabc30da03c5ec8870110ced0f7
Yagyesh <yagyeshyadav2005@gmail.com>
2025-07-19 23:49:56 +0530

    added header and SpecialityMenu pages

--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from '../components/Header';
+import SpecialityMenu from '../components/SpecialityMenu';
 
 const Home = () => {
   return (
     <div>
-        Home page
+      <Header/>
+      <SpecialityMenu/>
     </div>
   )
 }
